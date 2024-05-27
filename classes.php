2a1b43cd20cfe296832b4d2ae40ddb83c8634fc7
Diti2604 <ditiboyboy@gmail.com>
2024-05-27 09:50:29 +0200

    overriding

--- classes.php
+++ classes.php
@@ -20,28 +20,46 @@
 // $b -> param1 = 10;
 // $b -> operation1();
 
+//INHERITENCE
+
+// class A{
+// private function operation1(){
+// echo "operation1 called";
+// }
+
+// protected function operation2(){
+// echo "operation2 called";
+// }
+// public function operation3(){
+// echo "operation3 called";
+// }
+
+// }
+// class B extends A{
+// function __construct(){
+// // $this->operation1();
+// $this->operation2();
+// $this->operation3();
+// }}
+
+// $b = new B;
+// $b -> operation3();
+
+
+//OVERRIDING
 
 class A{
-private function operation1(){
-echo "operation1 called";
-}
-
-protected function operation2(){
-echo "operation2 called";
-}
-public function operation3(){
-echo "operation3 called";
-}
-
-}
-class B extends A{
-function __construct(){
-// $this->operation1();
-$this->operation2();
-$this->operation3();
+public $attribute = 'default value';
+function operation(){
+echo 'Something<br />';
+echo 'The value of $attribute is '. $this->attribute.'<br />';
 }}
-
-$b = new B;
-$b -> operation3();
-
+class B extends A{
+public $attribute = 'different value';
+function operation(){
+echo 'Something else<br />';
+echo 'The value of $attribute is '. $this->attribute.'<br />';
+}}
+$a = new A();
+$a->operation();
 ?>
