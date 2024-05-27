d527dd88b33e1864c9fc1734343b9e465b153017
Diti2604 <ditiboyboy@gmail.com>
2024-05-27 10:26:34 +0200

    small changes

--- classes.php
+++ classes.php
@@ -48,18 +48,20 @@
 
 //OVERRIDING
 
-class A{
-public $attribute = 'default value';
-function operation(){
-echo 'Something<br />';
-echo 'The value of $attribute is '. $this->attribute.'<br />';
-}}
-class B extends A{
-public $attribute = 'different value';
-function operation(){
-echo 'Something else<br />';
-echo 'The value of $attribute is '. $this->attribute.'<br />';
-}}
-$a = new A();
-$a->operation();
+// class A{
+// public $attribute = 'default value';
+// function operation(){
+// echo 'Something<br />';
+// echo 'The value of $attribute is '. $this->attribute.'<br />';
+// }}
+// class B extends A{
+// public $attribute = 'different value';
+// function operation(){
+// echo 'Something else<br />';
+// echo 'The value of $attribute is '. $this->attribute.'<br />';
+// }}
+// $a = new A();
+// $a->operation();
+
+
 ?>
